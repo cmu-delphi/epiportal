059905cc3beb3c3b80514a624df5ff9da7d0d6dc
Dmytro Trotsko <dmytro.trotsko@lvimac0084.local>
2025-12-02 23:25:31 +0200

    Fixed chart

--- src/alternative_interface/urls.py
+++ src/alternative_interface/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from alternative_interface.views import alternative_interface_view
+from alternative_interface.views import (
+    alternative_interface_view,
+    get_available_geos_ajax,
+    get_chart_data_ajax,
+)
 
 urlpatterns: list[URLPattern] = [
     path("alternative_interface", alternative_interface_view, name="alternative_interface"),
+    path("api/get_available_geos", get_available_geos_ajax, name="get_available_geos_ajax"),
+    path("api/get_chart_data", get_chart_data_ajax, name="get_chart_data_ajax"),
 ]
