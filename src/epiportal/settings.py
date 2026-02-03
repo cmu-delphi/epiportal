9357202e97fada70e4041f0f810096e7d9d10e3a
Dmytro Trotsko <dmytro.trotsko@lvimac0084.local>
2026-02-03 15:56:24 +0200

    version bump

--- src/epiportal/settings.py
+++ src/epiportal/settings.py
@@ -25,7 +25,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.redis import RedisIntegration
 
 APP_VERSION = "1.1.0"
-ALTERNATIVE_INTERFACE_VERSION = "1.0.10"
+ALTERNATIVE_INTERFACE_VERSION = "1.0.11"
 
 
 EPIVIS_URL = os.environ.get("EPIVIS_URL", "https://delphi.cmu.edu/epivis/")
