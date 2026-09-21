3db1a5385d15bfb5bf6ac94176d157d77b3ee9bb
Dmytro Trotsko <dmytrotsko@gmail.com>
2026-09-21 22:35:44 +0300

    Updated 'process_indicator_set' function to be more flexible about column name

--- src/indicators/resources.py
+++ src/indicators/resources.py
@@ -63,16 +63,16 @@ def process_pathogens(row) -> None:
     row["Pathogen/\nDisease Area"] = ",".join(str(el) for el in pathogen_ids)
 
 
-def process_indicator_set(row) -> None:
+def process_indicator_set(row, column_name="Indicator Set") -> None:
     indicator_set_id = None
-    if row["Indicator Set"]:
-        indicator_set_name = row["Indicator Set"].strip()
+    if row[column_name]:
+        indicator_set_name = row[column_name].strip()
         try:
             indicator_set_obj = IndicatorSet.objects.get(name=indicator_set_name)
             indicator_set_id = indicator_set_obj.id
         except IndicatorSet.DoesNotExist:
             logger.warning(f"Indicator Set '{indicator_set_name}' not found.")
-    row["Indicator Set"] = indicator_set_id
+    row[column_name] = indicator_set_id
 
 
 def process_indicator_type(row) -> None:
@@ -721,7 +721,7 @@ class USStateIndicatorResource(ModelResource):
         """Post-processes each row after importing."""
         strip_all_string_values(row)
         fix_boolean_fields(row)
-        process_indicator_set(row)
+        process_indicator_set(row, "Indicator Set Name")
 
     def after_save_instance(self, instance, row, **kwargs):
         instance.source_type = "us_state"
